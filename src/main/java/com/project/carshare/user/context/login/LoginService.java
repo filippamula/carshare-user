ad615705f2b60883fda2c57d496ea3b29c776561
filip <filippamula@onet.pl>
2023-07-02 19:47:03 +0200

    changed authentication process

--- src/main/java/com/project/carshare/user/context/login/LoginService.java
+++ src/main/java/com/project/carshare/user/context/login/LoginService.java
@@ -5,6 +5,8 @@ import com.project.carshare.user.context.login.dto.AuthorizationResponse;
 import com.project.carshare.user.context.login.dto.LoginRequest;
 import com.project.carshare.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +16,18 @@ public class LoginService {
 
     private final UserRepository userRepository;
     private final JwtService jwtService;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
     public AuthorizationResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
 
         var user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new RuntimeException("Invalid username or password"));
-
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())){
-            throw new RuntimeException("Invalid username or password");
-        }
-
+                .orElseThrow(() -> new RuntimeException("User not found"));
         var jwtToken = jwtService.generateToken(user);
         return AuthorizationResponse.builder()
                 .accessToken(jwtToken)
