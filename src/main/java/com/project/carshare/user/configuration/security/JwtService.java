357f0edc784a4bdf73d07cd679df6ecb66fb4625
filip <filippamula@onet.pl>
2023-07-09 18:38:50 +0200

    moved secret key to config file

--- src/main/java/com/project/carshare/user/configuration/security/JwtService.java
+++ src/main/java/com/project/carshare/user/configuration/security/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -17,7 +18,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "b2NjdXJjb29raWVzc3Rvb2Rwcm9iYWJseWNsb3RoZXNzdXBwZXJjYXJlZnVsbHljb3I=";
+    @Value("${secret}")
+    private String secretKey;
 
     public String extractUserId(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -73,7 +75,7 @@ public class JwtService {
     }
 
     private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
